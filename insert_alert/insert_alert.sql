11015ec3ca7ad869b17b9e8aa90089032e8cfb06
aida <aidaand96@gmail.com>
2024-04-09 10:26:25 +0330

    delet_alert

--- insert_alert/insert_alert.sql
+++ insert_alert/insert_alert.sql
@@ -16,3 +16,23 @@ begin
     values(new.client_id, new.date,new.amount,'insert',now());
 end $$
 delimiter ;
+
+
+
+--payments after delete
+delimiter $$
+
+drop trigger if exists pyments_after_delete; 
+
+create trigger pyments_after_delete
+	after delete on payments
+	for each row
+begin
+	update invoices
+    set payment_total=payment_total-old.amount
+    where invoice_id= old.invoice_id;
+    
+    insert into payments_audit
+    values(old.client_id, old.date,old.amount,'Delete',now());
+end $$
+delimiter ;
